bb2abf3498523a1fd5e43f13a5262a70483a10f2
mb <nerooren4001@gmail.com>
2023-11-24 20:11:32 +0300

    Add serializer, SubscriptionView and router

--- service/service/urls.py
+++ service/service/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from services.views import SubscriptionView
+
+router = DefaultRouter()
+router.register(r'api/subscriptions', SubscriptionView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
